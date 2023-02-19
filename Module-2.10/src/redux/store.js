d3dddb510346d95db381547f4c8c76a44284d6fd
neyaznafiz <neyaznafiz@outlook.com>
2023-02-20 03:15:22 +0600

    redux logger

--- Module-2.10/src/redux/store.js
+++ Module-2.10/src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
 
 export default store;
---
+++ Module-2.9/src/redux/middlewares/myLogger.js
@@ -0,0 +1,15 @@
+import rootReducer from "../rootReducer";
+
+// create middleware
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+  const upcomingState = [action].reduce(rootReducer, store.getState());
+  console.log(`Upcoming State : ${JSON.stringify(upcomingState)}`);
+
+  // pass the action
+  return next(action);
+};
+
+export default myLogger;
