c7e402442d847a526dd8c3519eda9a1b71fe5e77
neyaznafiz <neyaznafiz@outlook.com>
2023-02-20 02:59:49 +0600

    middleware created

---
+++ Module-2.9/src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./rootReducer";
+
+// create middleware
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+  const upcomingState = [action].reduce(rootReducer, store.getState());
+  console.log(`Upcoming State : ${JSON.stringify(upcomingState)}`);
+
+  // pass the action
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
+
+export default store;
