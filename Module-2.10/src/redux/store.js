160e24140c5564ea11fb1d9fd91eb12024c8227b
neyaznafiz <neyaznafiz@outlook.com>
2023-02-27 20:19:30 +0600

    upstreme

--- Module-2.10/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./rootReducer";
-import myLogger from "./middlewares/myLogger";
-import logger from "redux-logger";
-
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
-
-export default store;
--- Module-2.9/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./rootReducer";
-import myLogger from "./middlewares/myLogger";
-
-const store = createStore(rootReducer, applyMiddleware(myLogger));
-
-export default store;
